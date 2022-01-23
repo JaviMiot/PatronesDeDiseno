c32886e3ebed7a8c6c3442440ef8bbaa820e6e1e
Javier Manobanda <62907859+JaviManobanda@users.noreply.github.com>
2022-01-23 17:16:37 -0500

    Add firts class about class and interface in c#

---
+++ clases/clases/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FitnessPerson javi = new FitnessPerson("javier", 29);
+            Console.WriteLine(javi.showData());
+            Console.WriteLine(javi.run());
+            Console.ReadKey();
+        }
+
+      
+    }
+
+    public abstract class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string showData()
+        {
+            return "My name is " + this.name + " I'm " + this.age; 
+        }
+
+    }
+
+    public class FitnessPerson: Person
+    {
+        public FitnessPerson(string name, int age): base(name, age)
+        {
+          
+        }
+
+        public string run()
+        {
+            return "run";
+        }
+    }
+}
---
+++ interfaz/interfaz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pato1 = new Pato();
+            var pato2 = new Pato();
+            var pato3 = new Pato();
+            var pato4 = new Pato();
+            var pato5 = new Pato();
+
+            List<IVolador> patos = new List<IVolador>();
+
+            patos.Add(pato1);
+            patos.Add(pato2);
+            patos.Add(pato3);
+            patos.Add(pato4);
+            patos.Add(pato5);
+
+            void Avolar(List<IVolador> aves)
+            {
+                foreach (var ave in aves)
+                {
+                    ave.volar();
+                }
+            }
+
+
+            Avolar(patos);
+
+            Console.ReadKey();
+
+        }
+    }
+
+ 
+    interface IVolador
+    {
+        void volar();
+    }
+
+    interface ICaminador
+    {
+        void caminar();
+    }
+
+    public class  Pato: IVolador, ICaminador
+    {
+        public void volar()
+        {
+            Console.WriteLine("pato vuela");
+        }
+
+        public void caminar()
+        {
+            Console.WriteLine("pato camina");
+        }
+    }
+}
